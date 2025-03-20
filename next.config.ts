870583fe9ca5f8ae1cab7e19ccf02ee62f8065b0
cifelse <meetcif@gmail.com>
2025-03-20 10:56:08 +0800

    Apply service worker to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+/** DO NOT TOUCH */
+const withSerwist = withSerwistInit({
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+});
 
-export default nextConfig;
+export default withSerwist(nextConfig);
